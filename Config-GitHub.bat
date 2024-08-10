ec7c45ebf81e1619120e0dfe6e3c0a46dce72079
Marcos Lima <marcos-alexandre26@hotmail.com>
2024-08-09 21:27:40 -0300

    Aula 09-08-2024 - Programação WEB I

---
+++ Config-GitHub.bat
@@ -0,0 +1,12 @@
+echo OFF
+
+echo --- Configurando Úsuario GitHub ---
+echo
+echo
+git config --global user.name "Seu Nome"
+git config --global user.email seu@email.com
+echo
+echo
+echo --------------    FIM    --------------
+
+pause
